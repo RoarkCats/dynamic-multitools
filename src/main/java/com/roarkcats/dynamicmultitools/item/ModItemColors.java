190f09829e121d3b85fda3208ca9512e546ee668
RoarkCats <roark007games@gmail.com>
2026-07-30 22:48:35 -0700

    - Dyeable item

---
+++ src/main/java/com/roarkcats/dynamicmultitools/item/ModItemColors.java
@@ -0,0 +1,25 @@
+package com.roarkcats.dynamicmultitools.item;
+
+import net.minecraft.world.item.component.DyedItemColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+
+import static com.roarkcats.dynamicmultitools.DynamicMultitools.MODID;
+
+@EventBusSubscriber(modid = MODID) @OnlyIn(Dist.CLIENT)
+public class ModItemColors {
+
+    private static final int DEFAULT = -1;
+
+    @SubscribeEvent
+    public static void registerItemColorHandlers(RegisterColorHandlersEvent.Item event) {
+        // Parameters are the item stack and the tint index.
+        event.register((stack, tintIndex) -> tintIndex == 1 ? DyedItemColor.getOrDefault(stack, DEFAULT) : -1,
+                // items to tint
+                ModItems.DOLABRA
+        );
+    }
+}
--- src/main/java/com/roarkcats/dynamicmultitools/item/multitools/DolabraItem.java
+++ src/main/java/com/roarkcats/dynamicmultitools/item/multitools/DolabraItem.java
@@ -1,26 +1,32 @@
 package com.roarkcats.dynamicmultitools.item.multitools;
 
 import com.roarkcats.dynamicmultitools.util.Tags;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.component.DyedItemColor;
 import net.neoforged.neoforge.common.ItemAbilities;
 import net.neoforged.neoforge.common.ItemAbility;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static net.minecraft.core.component.DataComponents.DYED_COLOR;
+
 public class DolabraItem extends DiggerItem {
 
     public static final Set<ItemAbility> DEFAULT_DOLABRA_ACTIONS = new HashSet<>();
-    static {
+    static { // this all doesn't seem to do much...
         DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_PICKAXE_ACTIONS);
         DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_AXE_ACTIONS);
     }
 
-
     public DolabraItem(Tier tier, Properties properties) {
-        super(tier, Tags.Blocks.MINEABLE_WITH_DOLABRA, properties);
+        this(tier, properties, new DyedItemColor(-1, false));
+    }
+    public DolabraItem(Tier tier, Properties properties, DyedItemColor color) {
+        super(tier, Tags.Blocks.MINEABLE_WITH_DOLABRA, properties.component(DYED_COLOR, color));
     }
 
     public boolean canPerformAction(ItemStack stack, ItemAbility itemAbility) {
