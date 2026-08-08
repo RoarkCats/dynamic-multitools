e86ed9af198d8ee00f6e84f291562eae465ae605
RoarkCats <roark007games@gmail.com>
2026-08-07 17:48:04 -0700

    - Added working ItemAbilities (strip/till/scrape etc.)

--- src/main/java/com/roarkcats/dynamicmultitools/item/custom/DynamicDiggerItem.java
+++ src/main/java/com/roarkcats/dynamicmultitools/item/custom/DynamicDiggerItem.java
@@ -1,8 +1,14 @@
 package com.roarkcats.dynamicmultitools.item.custom;
 
 import com.roarkcats.dynamicmultitools.datapack.DynamicTier;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
@@ -10,14 +16,20 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LevelEvent;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import static com.roarkcats.dynamicmultitools.DynamicMultitools.LOGGER;
 
 public class DynamicDiggerItem extends DynamicTieredItem {
 
@@ -62,5 +74,70 @@ public class DynamicDiggerItem extends DynamicTieredItem {
 
     public void postHurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
         stack.hurtAndBreak(2, attacker, EquipmentSlot.MAINHAND);
+    }
+
+    // -- Item Abilities --
+
+    // Override this with ability set
+    public Set<ItemAbility> getItemAbilities() {
+        return Set.of();
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        for (ItemAbility ability : getItemAbilities()) {
+            InteractionResult result = tryPerformToolAction(context, ability);
+            if (result != InteractionResult.PASS) return result;
+        }
+        return super.useOn(context);
+    }
+
+    // Item Ability helpers
+    protected InteractionResult tryPerformToolAction(UseOnContext context, ItemAbility ability) {
+        var level = context.getLevel();
+        var pos = context.getClickedPos();
+        var player = context.getPlayer();
+        var state = level.getBlockState(pos);
+        var item = context.getItemInHand();
+
+        // Query NeoForge's built-in tool action handlers
+        var modifiedState = state.getToolModifiedState(context, ability, false);
+        if (modifiedState != null) {
+            // Play sound & handle block replacement
+            playSoundFor(context, ability);
+            particlesFor(context, ability);
+
+            level.setBlock(pos, modifiedState, Block.UPDATE_ALL_IMMEDIATE);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, modifiedState)); // sculk sensor
+
+            if (player instanceof ServerPlayer) CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer)player, pos, item);
+            if (player != null) item.hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
+
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return InteractionResult.PASS;
+    }
+
+    protected void playSoundFor(UseOnContext context, ItemAbility ability) {
+        SoundEvent sound = null;
+        if (ability.equals(ItemAbilities.AXE_STRIP)) sound = SoundEvents.AXE_STRIP;
+        else if (ability.equals(ItemAbilities.AXE_SCRAPE)) sound = SoundEvents.AXE_SCRAPE;
+        else if (ability.equals(ItemAbilities.AXE_WAX_OFF)) sound = SoundEvents.AXE_WAX_OFF;
+        else if (ability.equals(ItemAbilities.HOE_TILL)) sound = SoundEvents.HOE_TILL;
+        else if (ability.equals(ItemAbilities.SHOVEL_FLATTEN)) sound = SoundEvents.SHOVEL_FLATTEN;
+        else if (ability.equals(ItemAbilities.SHOVEL_DOUSE)) sound = SoundEvents.GENERIC_EXTINGUISH_FIRE;
+
+        if (sound != null) context.getLevel().playSound(context.getPlayer(), context.getClickedPos(), sound, SoundSource.BLOCKS, 1.0F, 1.0F);
+        else LOGGER.debug("Unknown sound missing definition for ItemAbility: {}", ability);
+    }
+
+    protected void particlesFor(UseOnContext context, ItemAbility ability) {
+        int levelEvent = 0;
+        if (ability.equals(ItemAbilities.AXE_SCRAPE)) levelEvent = LevelEvent.PARTICLES_SCRAPE;
+        else if (ability.equals(ItemAbilities.AXE_WAX_OFF)) levelEvent = LevelEvent.PARTICLES_WAX_OFF;
+//        else if (ability.equals(ItemAbilities.HOE_TILL)) levelEvent = LevelEvent.PARTICLES_DESTROY_BLOCK;
+//        else if (ability.equals(ItemAbilities.SHOVEL_DOUSE)) levelEvent = LevelEvent.PARTICLES_SHOOT_SMOKE;
+
+        if (levelEvent != 0) context.getLevel().levelEvent(context.getPlayer(), levelEvent, context.getClickedPos(), 0);
     }
 }
--- src/main/java/com/roarkcats/dynamicmultitools/item/custom/multitools/DolabraItem.java
+++ src/main/java/com/roarkcats/dynamicmultitools/item/custom/multitools/DolabraItem.java
@@ -6,23 +6,20 @@ import com.roarkcats.dynamicmultitools.item.custom.DynamicDiggerItem;
 import com.roarkcats.dynamicmultitools.item.custom.DynamicTieredItem;
 import com.roarkcats.dynamicmultitools.util.Tags;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.DyedItemColor;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Tool;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static com.roarkcats.dynamicmultitools.DynamicMultitools.LOGGER;
 import static net.minecraft.core.component.DataComponents.ATTRIBUTE_MODIFIERS;
 import static net.minecraft.core.component.DataComponents.TOOL;
 
 public class DolabraItem extends DynamicDiggerItem {
-
-//    public static final Set<ItemAbility> DEFAULT_DOLABRA_ACTIONS = new HashSet<>();
-//    static { // this all doesn't seem to do much...
-//        DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_PICKAXE_ACTIONS);
-//        DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_AXE_ACTIONS);
-//    }
 
     public DolabraItem(DynamicTier tier, Properties properties) {
         super(tier, dolabraTool(tier), properties.attributes(dolabraAttributes(tier)));
@@ -51,7 +48,21 @@ public class DolabraItem extends DynamicDiggerItem {
         return itemStack;
     }
 
-//    public boolean canPerformAction(ItemStack stack, ItemAbility itemAbility) {
-//        return DEFAULT_DOLABRA_ACTIONS.contains(itemAbility);
-//    }
+    // Actions
+    public static final Set<ItemAbility> DEFAULT_DOLABRA_ACTIONS = new HashSet<>();
+    static {
+        DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_PICKAXE_ACTIONS);
+        DEFAULT_DOLABRA_ACTIONS.addAll(ItemAbilities.DEFAULT_AXE_ACTIONS);
+    }
+
+    @Override
+    public Set<ItemAbility> getItemAbilities() {
+        return DEFAULT_DOLABRA_ACTIONS;
+    }
+
+    @Override
+    public boolean canPerformAction(ItemStack stack, ItemAbility itemAbility) {
+        return DEFAULT_DOLABRA_ACTIONS.contains(itemAbility);
+    }
+
 }
